e6a993b91a6b5df5c0847f7890c366c4e2810be6
jbeauchemin <janic_33@msn.com>
2015-01-05 14:34:20 -0500

    add sequence + basic lvling

--- client/js/main.js
+++ client/js/main.js
@@ -1,30 +1,13 @@
 Session.setDefault("counter", 0);
 Session.setDefault("topScore", 0);
 Session.setDefault("gameActive", 0);
+Session.setDefault("currentSequence", []);
 
+var userSequence = [];
 var sequenceGenerated = false;
 
 Template.main.rendered = function () {
     //Meteor.call('removeAllScores');
-
-    Meteor.call('getUserTopScore', Meteor.userId(), function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-
-        if (!data) {
-            var userData = Meteor.users.findOne({_id: Meteor.userId()});
-
-            Scores.insert({
-                score: Session.get("counter"),
-                time: new Date(),
-                owner: Meteor.userId(),
-                username: userData.username
-            });
-        } else {
-            Session.set("topScore", data);
-        }
-    });
 };
 
 Template.main.helpers({
@@ -46,6 +29,10 @@ Template.main.helpers({
 
     scores: function () {
         return Scores.find({});
+    },
+
+    currentSequence: function () {
+        return Session.get("currentSequence");
     }
 });
 
@@ -54,23 +41,59 @@ Template.main.events({
         Session.set("gameActive", !Session.get("gameActive"));
 
         if (!sequenceGenerated) {
-            Meteor.call('createSequence', Meteor.userId(), $('.item').length, Session.get("counter") + 1, function (err, data) {
-                if (err) { return console.log(err); }
-                sequenceGenerated = true;
+            Meteor.call('getUserTopScore', Meteor.userId(), function (err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                if (data == null) {
+                    var userData = Meteor.users.findOne({_id: Meteor.userId()});
+
+                    Scores.insert({
+                        score: Session.get("counter"),
+                        time: new Date(),
+                        owner: Meteor.userId(),
+                        username: userData.username
+                    }, function () {
+                        Meteor.call('createSequence', Meteor.userId(), $('.item').length, Session.get("counter") + 1, function (err, data) {
+                            if (err) { return console.log(err); }
+                            Session.set("currentSequence", data);
+                            sequenceGenerated = true;
+                        });
+                    });
+                } else {
+                    Session.set("topScore", data);
+
+                    Meteor.call('createSequence', Meteor.userId(), $('.item').length, Session.get("counter") + 1, function (err, data) {
+                        if (err) { return console.log(err); }
+                        Session.set("currentSequence", data);
+                        sequenceGenerated = true;
+                    });
+                }
             });
         }
     },
 
-    'click .item': function () {
+    'click .item': function (event) {
         if (!Session.get("gameActive")) { return; }
-        //Session.set("counter", Session.get("counter") + 1);
-        //Meteor.call('updateScore', Meteor.userId(), Session.get("counter"));
-        //
-        //if (Session.get("counter") > Session.get("topScore")) {
-        //    Session.set("topScore", Session.get("counter"));
-        //}
 
-        var sequence = Scores.findOne({owner: Meteor.userId()}).currentSequence;
+        //var sequence = Scores.findOne({owner: Meteor.userId()}).currentSequence;
+        userSequence.push($(event.target).index());
+
+        if (userSequence.equals(Session.get("currentSequence"))) {
+            Session.set("counter", Session.get("counter") + 1);
+            Meteor.call('updateScore', Meteor.userId(), Session.get("counter"));
+
+            if (Session.get("counter") > Session.get("topScore")) {
+                Session.set("topScore", Session.get("counter"));
+            }
+
+            Session.set("gameActive", !Session.get("gameActive"));
+            Session.set("currentSequence", []);
+
+            userSequence = [];
+            sequenceGenerated = false;
+        }
     }
 });
 
